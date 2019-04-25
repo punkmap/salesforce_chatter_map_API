d2305b0a9879bcb2025d2fcc40df139539023eb2
CARYNET\twaring <tyler.waring@townofcary.org>
2019-04-25 09:26:50 -0400

    First commit of the salesforce_chatter_map_API

---
+++ config.example.js
@@ -0,0 +1,12 @@
+let config = {}
+
+config.salesforce = {}
+config.salesforce.client_id = "salesforce_connected_app_clientID_goes_here"
+config.salesforce.client_secret = "salesforce_connected_app_clientSecret_goes_here"
+config.salesforce.sandboxRoot = 'https://carync--your_sandbox_name.cs51.my.salesforce.com/'
+config.salesforce.callbackRoot = 'http://yourCallback.com'
+
+config.app={}
+config.app.port = 3001 //or whatever port you want to run this applicaiton on. 
+
+module.exports = config
