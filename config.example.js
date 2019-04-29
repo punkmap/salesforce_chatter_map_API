359f33fbec1d70a3686c78f0af29acc71a63f770
CARYNET\twaring <tyler.waring@townofcary.org>
2019-04-29 08:33:08 -0400

    update example.config

--- config.example.js
+++ config.example.js
@@ -3,7 +3,7 @@ let config = {}
 config.salesforce = {}
 config.salesforce.client_id = "salesforce_connected_app_clientID_goes_here"
 config.salesforce.client_secret = "salesforce_connected_app_clientSecret_goes_here"
-config.salesforce.sandboxRoot = 'https://carync--your_sandbox_name.cs51.my.salesforce.com/'
+config.salesforce.sandboxRoot = 'https://your_sandbox_name.cs51.my.salesforce.com/'
 config.salesforce.callbackRoot = 'http://yourCallback.com'
 
 config.app={}
